4dc9b0524d212a03bda6ff1351458a936ce75931
Mads Hougesen <mads@mhouge.dk>
2023-12-16 02:18:00 +0100

    fix: use my own domain for user agent

--- server/api/github/repositories/index.ts
+++ server/api/github/repositories/index.ts
@@ -25,7 +25,7 @@ export default defineEventHandler(async (event): Promise<Project[]> => {
       method: 'POST',
       headers: {
         authorization: `Bearer ${config.githubApiKey}`,
-        'user-agent': 'MadsHougesen +http://yandex.com/bots',
+        'user-agent': 'MadsHougesen +http://mhouge.dk',
       },
       body: {
         query: `
