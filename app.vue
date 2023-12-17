7226cc14c7ada6ce1554f192d47b1507fe908ea2
Mads Hougesen <mads@mhouge.dk>
2023-12-17 19:58:09 +0100

    feat: add meta tags

--- app.vue
+++ app.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+const baseTitle = 'Mads Hougesen | Software Developer';
+const baseDescription =
+  'Software developer from Denmark. Lover of all things programming - but  always learning new stuff.';
+const baseOgImage = '/og-image.png';
+
+const titleTemplate = (title?: string) => {
+  if (title?.length) {
+    return `${title} | Mads Hougesen`;
+  }
+  return baseTitle;
+};
+
+useServerSeoMeta({
+  title: baseTitle,
+  titleTemplate,
+  author: 'Mads Hougesen',
+  description: baseDescription,
+  ogTitle: baseTitle,
+  ogDescription: baseDescription,
+  ogImage: baseOgImage,
+  twitterCard: 'summary_large_image',
+  twitterCreator: 'mads_hougesen',
+  ogSiteName: 'Mads Hougesen',
+});
+</script>
+
 <template>
   <div class="container mx-auto h-full min-h-screen px-8 py-4">
     <Navigation />
