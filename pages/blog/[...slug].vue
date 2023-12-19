6b625aa637153b2996bb13a767c0b2506bf0fe80
Mads Hougesen <mads@mhouge.dk>
2023-12-20 00:04:59 +0100

    style: add author and date to blog post

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,15 @@
 <template>
-  <article class="prose mx-auto rounded-lg p-8 dark:prose-invert lg:prose-xl">
-    <ContentDoc />
-  </article>
+  <ContentDoc v-slot="{ doc }">
+    <article class="prose mx-auto rounded-lg p-8 dark:prose-invert lg:prose-xl">
+      <h1>{{ doc.title }}</h1>
+
+      <p class="font-medium">
+        By Mads Hougesen<template v-if="doc['date created']?.length"
+          >, {{ new Date(doc['date created']).toDateString() }}
+        </template>
+      </p>
+
+      <ContentRenderer :value="doc" />
+    </article>
+  </ContentDoc>
 </template>
