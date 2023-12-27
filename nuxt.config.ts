7794f655e352cc0e410a3115a0cd4cd8704b2659
Mads Hougesen <mads@mhouge.dk>
2023-12-27 15:08:49 +0100

    style: switch font to Inter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,23 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+        },
+      ],
+    },
+  },
+
   nitro: {
     prerender: {
       autoSubfolderIndex: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,18 @@ export default {
         black: { primary: '#101010', secondary: '#151515' },
         white: { primary: '#f7f7f7', secondary: '#eeeeee' },
       },
+      fontFamily: {
+        sans: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
