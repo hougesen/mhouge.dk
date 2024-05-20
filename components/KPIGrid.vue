93386b93a068dc7c6c39080b599332f27081c396
Mads Hougesen <mads@mhouge.dk>
2024-05-20 22:21:07 +0200

    feat: show leetcode metrics

---
+++ components/KPIGrid.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 2xl:grid-cols-4">
+    <slot />
+  </div>
+</template>
---
+++ pages/leetcode/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const { data } = useFetch('/api/leetcode/stats');
+</script>
+
+<template>
+  <div class="flex w-full flex-col gap-12 pb-8">
+    <section class="flex flex-col gap-8">
+      <SectionTitle> Leetcode Metrics </SectionTitle>
+
+      <a
+        class="text-dark-primary text-xl underline dark:text-white-primary"
+        href="https://leetcode.com/u/hougesen"
+        rel="noreferrer noopener"
+        target="_blank"
+        >Account</a
+      >
+
+      <KPIGrid>
+        <KPI
+          label="Total"
+          :value="
+            data?.all?.count ||
+            (data?.easy?.count ?? 0) +
+              (data?.medium?.count ?? 0) +
+              (data?.hard?.count ?? 0)
+          "
+        />
+
+        <KPI label="Easy" :value="data?.easy?.count ?? 0" />
+
+        <KPI label="Medium" :value="data?.medium?.count ?? 0" />
+
+        <KPI label="Hard" :value="data?.hard?.count ?? 0" />
+      </KPIGrid>
+    </section>
+  </div>
+</template>
