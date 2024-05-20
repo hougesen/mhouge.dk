98ce1b21a3f2b0f7b89d7d5069219bd605c76e1f
Mads Hougesen <mads@mhouge.dk>
2024-05-20 19:31:06 +0200

    feat: list oss contributions (#113)

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,13 +2,14 @@
 import type { Project } from '~/github';
 
 defineProps<{
+  title?: string;
   projects: Project[];
 }>();
 </script>
 
 <template>
   <section v-if="projects?.length" id="projects" class="flex flex-col gap-8">
-    <SectionTitle>Projects</SectionTitle>
+    <SectionTitle>{{ title || 'Projects' }}</SectionTitle>
 
     <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
       <Project
---
+++ pages/contributed-to/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: 'Open Source contributions this year',
+});
+
+useServerSeoMeta({
+  title: 'Open Source contributions this year',
+});
+
+const { data: repositories } = useFetch('/api/github/contributions');
+</script>
+
+<template>
+  <div>
+    <Projects
+      :projects="repositories ?? []"
+      title="Open Source contributions this year"
+    />
+  </div>
+</template>
