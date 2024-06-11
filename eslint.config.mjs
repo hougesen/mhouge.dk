5bf636339aede1a1651ccb80372a114300b63ad0
Mads Hougesen <mads@mhouge.dk>
2024-06-11 03:30:16 +0200

    chore(eslint): migrate to eslint 9 (#140)

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+// @ts-expect-error no type declaration?
+import tailwind from 'eslint-plugin-tailwindcss';
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(tailwind.configs['flat/recommended'])
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/html-self-closing': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  })
+  .override('tailwindcss:rules', {
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  });
